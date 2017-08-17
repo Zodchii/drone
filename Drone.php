7832c4e116148f4b15d1786d3a8ea7a04446b5b8
maximr <maximr@spotoption.com>
2017-08-17 16:52:39 +0300

    hello word2

--- Drone.php
+++ Drone.php
@@ -1,14 +1,67 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: maximr
- * Date: 8/17/2017
- * Time: 4:42 PM
- */
 
+namespace drone;
+/**
+ * Class Drone
+ * @package drone
+ */
 class Drone
 {
- public function __construct(){
+    private $distance;
+    private $height;
 
- }
+    public function __construct()
+    {
+
+    }
+
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+
+    }
+
+    public function left()
+    {
+
+    }
+
+    public function right()
+    {
+
+    }
+
+    public function faster()
+    {
+
+    }
+
+    public function slower()
+    {
+
+    }
+
+    public function takePicture()
+    {
+
+    }
+
+    public function followLead($distance, $height)
+    {
+        $this->distance = $distance;
+        $this->height = $height;
+
+    }
+
+    public function exitLead()
+    {
+        $this->distance = 0;
+        $this->height = 0;
+    }
+
+
 }
---
+++ readMe.txt
@@ -0,0 +1,13 @@
+The drone should have a dynamic number of engines.
+He should be able to receive commands from a remote control:
+● Up
+● Down
+● Left
+● Right
+● Faster
+● Slower
+● Take picture
+● Enter “follow the leader” mode (see below)
+● Exit “follow the leader” mode
+In “follow the leader” mode the drone will input distance and height, and will keep this distance
+and height from the control.
