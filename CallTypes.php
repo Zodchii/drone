b87648010c2e5ee6381c62adfca1550248f9d35b
maximr <maximr@spotoption.com>
2017-08-17 17:12:03 +0300

    hello word3

---
+++ CallTypes.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Drone;
+/**
+ * Class CallTypes
+ * @package Drone
+ */
+
+class CallTypes
+{
+    const UP = 'up';
+    const DOWN = 'down';
+    const LEFT = 'left';
+    const RIGHT = 'right';
+    const SPEED_STEP = 1;
+}
--- Drone.php
+++ Drone.php
@@ -1,14 +1,16 @@
 <?php
 
-namespace drone;
+namespace Drone;
 /**
  * Class Drone
  * @package drone
  */
 class Drone
 {
-    private $distance;
-    private $height;
+    private $distance = 0;
+    private $height = 0;
+    private $speed = 0;
+    private $direction;
 
     public function __construct()
     {
@@ -17,50 +19,60 @@ class Drone
 
     public function up()
     {
-
+        $this->direction = CallTypes::UP;
+        return $this;
     }
 
     public function down()
     {
-
+        $this->direction = CallTypes::DOWN;
+        return $this;
     }
 
     public function left()
     {
-
+        $this->direction = CallTypes::LEFT;
+        return $this;
     }
 
     public function right()
     {
-
+        $this->direction = CallTypes::RIGHT;
+        return $this;
     }
 
     public function faster()
     {
-
+        $this->speed += CallTypes::SPEED_STEP;
+        return $this;
     }
 
     public function slower()
     {
-
+        $this->speed -= CallTypes::SPEED_STEP;
+        return $this;
     }
 
     public function takePicture()
     {
-
+        return $this;
     }
 
     public function followLead($distance, $height)
     {
+        if (!$this->speed) {
+            throw new \Exception('Dron is off');
+        }
         $this->distance = $distance;
         $this->height = $height;
-
+        return $this;
     }
 
     public function exitLead()
     {
         $this->distance = 0;
         $this->height = 0;
+        return $this;
     }
 
 
